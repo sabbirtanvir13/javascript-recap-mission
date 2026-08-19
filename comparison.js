e410c263e1ab98d2c5e4d7b95b098901bce2ae6d
sabbirtanvir13 <sabbirtanvirahmed18@gmail.com>
2026-08-19 10:22:38 +0600

    first commit

---
+++ comparison.js
@@ -0,0 +1,10 @@
+ 
+// | -------------------- | --------------------------- | - |
+// | `==`                 | Equal to                    |   |
+// | `===`                | Strict Equal                |   |
+// | `!=`                 | Not Equal                   |   |
+// | `!==`                | Strict Not Equal            |   |
+// | `>`                  | Greater than                |   |
+// | `<`                  | Less than                   |   |
+// | `>=`                 | Greater than or Equal       |   |
+// | `<=`                 | Less than or Equal EIBHABE  |   |
